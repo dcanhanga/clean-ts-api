051f5d22811b28650036026b350417cd1d2796ea
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2024-01-30 12:44:02 +0100

    feat: ensure SignUpController returns 400  if no password confirmation  is provided

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -4,7 +4,7 @@ import { type IHttpResponse, type IHttpRequest } from '../protocols/http';
 
 export class SignUpController {
   handle(httpRequest: IHttpRequest<any>): IHttpResponse<any> {
-    const requiredFields = ['name', 'email', 'password'];
+    const requiredFields = ['name', 'email', 'password', 'password_confirmation'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
