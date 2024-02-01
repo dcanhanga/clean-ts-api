3daddedab91d6f9d8ba301b13cc8dccc7308abe5
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2024-02-01 11:46:03 +0100

    feat: ensure  SingUp Controler retun 500 if EmailValidotor throws

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-erro';
 import { MissingParamError } from '../errors/missing-param-erro';
+import { ServerError } from '../errors/server-error';
 import { badRequest } from '../helpers/http-helper';
 import { type IController } from '../protocols/controller';
 import { type IEmailValidator } from '../protocols/email-validator';
@@ -7,22 +8,31 @@ import { type IHttpResponse, type IHttpRequest } from '../protocols/http';
 
 export class SignUpController implements IController<any> {
   constructor(private readonly emailValidator: IEmailValidator) {}
-  handle(httpRequest: IHttpRequest<any>): IHttpResponse<any> {
-    const requiredFields = ['name', 'email', 'password', 'password_confirmation'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
-      }
-    }
-    const email: string = typeof httpRequest?.body.email === 'string' ? httpRequest.body.email : '';
-    const isValid = this.emailValidator.isValid(email);
 
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'));
+  handle(httpRequest: IHttpRequest<any>): IHttpResponse<any> {
+    try {
+      const requiredFields = ['name', 'email', 'password', 'password_confirmation'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+      const email: string =
+        typeof httpRequest?.body.email === 'string' ? httpRequest.body.email : '';
+      const isValid = this.emailValidator.isValid(email);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      return {
+        statusCode: 200,
+        body: { message: 'User created successfully' }
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      };
     }
-    return {
-      statusCode: 200,
-      body: { message: 'User created successfully' }
-    };
   }
 }
