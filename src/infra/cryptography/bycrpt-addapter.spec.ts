28df990c696f4e1ee36b2724043f7507cd8ea12a
Domingos Canhanga <canhanga96@gmail.com>
2024-03-14 10:49:41 +0100

    test: should return a hash on success

--- src/infra/cryptography/bycrpt-addapter.spec.ts
+++ src/infra/cryptography/bycrpt-addapter.spec.ts
@@ -2,11 +2,23 @@ import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
 describe('Bcrypt Adapter', () => {
+  jest.mock('bcrypt', () => ({
+    async async(): Promise<string> {
+      return await Promise.resolve('$2b$12$ELRiGTH7XAPkVwgR404WIOmI1UTpGpyLAMhXIy6IS.55e7lfAi4ty');
+    }
+  }));
   test('Should call bcrypt with correct value', async () => {
     const saltOrRounds = 12;
     const sut = new BcryptAdapter(saltOrRounds);
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', saltOrRounds);
+  });
+  test('Should return a hash on success', async () => {
+    const saltOrRounds = 12;
+    const sut = new BcryptAdapter(saltOrRounds);
+
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe(hash);
   });
 });
