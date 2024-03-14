860b99a6d147f78a0d0ff505177f371dee09a93a
Domingos Canhanga <canhanga96@gmail.com>
2024-03-14 15:33:43 +0100

    feat: ensure AccountMongoRepository insert account to mongodb

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,39 @@
+import { type Collection, MongoClient, type ObjectId } from 'mongodb';
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export abstract class MongoHelper {
+  private static client: null | MongoClient = null;
+
+  protected constructor() {}
+
+  static async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(url);
+  }
+
+  static async disconnect(): Promise<void> {
+    if (this.client !== null) {
+      await this.client.close();
+      this.client = null;
+    }
+  }
+
+  static getCollection(name: string): Collection {
+    if (this.client === null) {
+      throw new Error('Não conectado ao banco de dados.');
+    }
+    return this.client.db().collection(name);
+  }
+
+  static map<T>(collection: unknown): T {
+    const isKeyValueObject =
+      collection !== null &&
+      typeof collection === 'object' &&
+      !Array.isArray(collection) &&
+      typeof collection !== 'function';
+    if (isKeyValueObject) {
+      const { _id, ...collectionWithoutId } = collection as { _id: ObjectId };
+      return { ...collectionWithoutId, id: _id.toString() } as unknown as T;
+    }
+    throw new Error('A coleção fornecida não é um objeto válido.');
+  }
+}
