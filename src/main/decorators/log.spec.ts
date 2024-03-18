75b2f9caa33d0608d3dc9f352e949acc8fb12dc2
Domingos Canhanga <canhanga96@gmail.com>
2024-03-18 16:00:56 +0100

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -6,7 +6,7 @@ interface ISutType {
   sut: LogControllerDecorator;
   controllerStub: IController;
 }
-const makeSut = (): ISutType => {
+const makeControllerStub = (): IController => {
   class ControllerStub implements IController {
     async handle(request: IHttpRequest): Promise<IHttpResponse> {
       return await Promise.resolve({
@@ -20,7 +20,10 @@ const makeSut = (): ISutType => {
       });
     }
   }
-  const controllerStub = new ControllerStub();
+  return new ControllerStub();
+};
+const makeSut = (): ISutType => {
+  const controllerStub = makeControllerStub();
   const sut = new LogControllerDecorator(controllerStub);
   return { sut, controllerStub };
 };
@@ -38,5 +41,27 @@ describe('LogController Decorator', () => {
     };
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+  test('Should return the same result of controller ', async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'password'
+      }
+    });
   });
 });
