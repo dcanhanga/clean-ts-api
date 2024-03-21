7ae33e5ac0524d670d597260823c828d49a3777e
Domingos Canhanga <canhanga96@gmail.com>
2024-03-21 17:24:30 +0100

    refactor: Create sut

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -1,14 +1,16 @@
 import { MissingParamError } from '../../errors';
 import { RequiredFieldsValidation } from './required-fields-validation';
-
+const makeSut = (): RequiredFieldsValidation => {
+  return new RequiredFieldsValidation('field');
+};
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldsValidation('field');
+    const sut = makeSut();
     const error = sut.validate({ name: 'any_name' });
     expect(error).toEqual(new MissingParamError('field'));
   });
   test('Should not return if validation succeeds', () => {
-    const sut = new RequiredFieldsValidation('field');
+    const sut = makeSut();
     const error = sut.validate({ field: 'any_value' });
     expect(error).toBeFalsy();
   });
