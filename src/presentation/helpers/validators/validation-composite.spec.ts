92f64bf5b2785076ef4736fb44c3bae5848709c2
Domingos Canhanga <canhanga96@gmail.com>
2024-03-22 10:35:29 +0100

    test: ensure ValidationComposite return the first error if more then one validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -3,7 +3,7 @@ import { type IValidation } from './validation';
 import { ValidationComposite } from './validation-composite';
 interface ISutTypes {
   sut: ValidationComposite;
-  validationStub: IValidation;
+  validationStubs: IValidation[];
 }
 const makeValidation = (): IValidation => {
   class ValidationStub implements IValidation {
@@ -14,15 +14,22 @@ const makeValidation = (): IValidation => {
   return new ValidationStub();
 };
 const makeSut = (): ISutTypes => {
-  const validationStub = makeValidation();
-  const sut = new ValidationComposite([validationStub]);
-  return { sut, validationStub };
+  const validationStubs = [makeValidation(), makeValidation()];
+  const sut = new ValidationComposite(validationStubs);
+  return { sut, validationStubs };
 };
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut();
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'));
+    const { sut, validationStubs } = makeSut();
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
+  });
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut();
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error());
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'));
+    const error = sut.validate({ field: 'any_value' });
+    expect(error).toEqual(new Error());
   });
 });
